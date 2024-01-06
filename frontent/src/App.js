ca4e82183ce2ac511ba5f45941a3ae0bc87fcd81
adama diouf <adamadiouf2017@gmail.com>
2024-01-06 04:38:51 -0600

    first commit

---
+++ frontent/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import{BrowserRouter,Routes,Route} from "react-router-dom"
+import Navbar from './components/Navbar'
+import Login from './pages/Login'
+import Register from './pages/Register'
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ frontent/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+
+function ColorSchemesExample() {
+  return (
+    <>
+      <Navbar bg='light' data-bs-theme='light'>
+        <Container>
+          <Navbar.Brand as={Link} to='/'>
+            Logo
+          </Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link as={Link} to='/home'>
+              Home
+            </Nav.Link>
+            
+            
+            <Nav.Link as={Link} to='/login'>
+              Login
+            </Nav.Link>
+            <Nav.Link as={Link} to='/register'>
+              Register
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default ColorSchemesExample
